8b1634f44bbf8ca63d5e3363b88891a065812644
unknown <abhinaykatta97@gmail.com>
2024-01-25 17:59:00 +0530

    initial commit with resources

---
+++ android/app/src/main/kotlin/com/example/fe_interview_task_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fe_interview_task_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:fe_interview_task_1/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
