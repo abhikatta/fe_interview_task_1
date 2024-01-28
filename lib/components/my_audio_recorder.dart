c243839ee2d70cdf3595d3591e9cf91e01e69cca
unknown <abhinaykatta97@gmail.com>
2024-01-28 20:18:31 +0530

    added not-recording waveform

--- lib/components/my_audio_recorder.dart
+++ lib/components/my_audio_recorder.dart
@@ -203,170 +203,186 @@ if not playing : button state::isPausable=false
 
   @override
   Widget build(BuildContext context) {
-    buttonState = buttonState;
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            margin: const EdgeInsets.only(bottom: 10),
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 2.76,
-                color: Colors.black,
-              ),
-              borderRadius: BorderRadius.circular(999),
-            ),
-            child: const MyAvatar(imagePath: 'lib/assets/human.png'),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(999),
-              border: const Border.symmetric(
-                horizontal: BorderSide(
-                  width: 3.5,
+    return Positioned(
+        bottom: 20,
+        left: 0,
+        right: 0,
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(bottom: 10),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 2.76,
                   color: Colors.black,
                 ),
-                vertical: BorderSide(
-                  width: 3,
-                  color: Colors.black,
-                ),
+                borderRadius: BorderRadius.circular(999),
               ),
+              child: const MyAvatar(imagePath: 'lib/assets/human.png'),
             ),
-            child: const Text(
-              'Stroll Question',
-              style: TextStyle(
-                fontSize: 11,
-                backgroundColor: Colors.black,
-                color: Color(0xFFF5F5F5),
-                fontWeight: FontWeight.w700,
-                fontFamily: 'ProximaNova',
-              ),
-            ),
-          ),
-          Container(
-              alignment: Alignment.center,
-              child: Column(children: [
-                const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 55),
-                    child: Text(
-                      'What is your favorite time of the day?',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 24,
-                        color: Color(0xFFF5F5F5),
-                        fontWeight: FontWeight.w700,
-                        fontFamily: 'ProximaNova',
-                      ),
-                    )),
-                const Text(
-                  '“Mine is definitely the peace in the morning.”',
-                  style: TextStyle(
-                    fontSize: 13,
-                    color: Color.fromRGBO(203, 201, 255, 0.7),
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.w400,
-                    fontFamily: 'ProximaNova',
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(999),
+                border: const Border.symmetric(
+                  horizontal: BorderSide(
+                    width: 3.5,
+                    color: Colors.black,
+                  ),
+                  vertical: BorderSide(
+                    width: 3,
+                    color: Colors.black,
                   ),
                 ),
-                Padding(
-                    padding: const EdgeInsets.only(
-                      top: 34,
+              ),
+              child: const Text(
+                'Stroll Question',
+                style: TextStyle(
+                  fontSize: 11,
+                  backgroundColor: Colors.black,
+                  color: Color(0xFFF5F5F5),
+                  fontWeight: FontWeight.w700,
+                  fontFamily: 'ProximaNova',
+                ),
+              ),
+            ),
+            Container(
+                alignment: Alignment.center,
+                child: Column(children: [
+                  const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 55),
+                      child: Text(
+                        'What is your favorite time of the day?',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Color(0xFFF5F5F5),
+                          fontWeight: FontWeight.w700,
+                          fontFamily: 'ProximaNova',
+                        ),
+                      )),
+                  const Text(
+                    '“Mine is definitely the peace in the morning.”',
+                    style: TextStyle(
+                      fontSize: 13,
+                      color: Color.fromRGBO(203, 201, 255, 0.7),
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w400,
+                      fontFamily: 'ProximaNova',
                     ),
-                    child: Text(
-                      totalDuration,
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(
-                        fontSize: 13,
-                        color: Color.fromRGBO(245, 245, 245, 1),
-                        fontWeight: FontWeight.w400,
-                        fontFamily: 'ProximaNova',
+                  ),
+                  Padding(
+                      padding: const EdgeInsets.only(
+                        top: 34,
                       ),
-                    )),
-              ])),
-          Row(
-              mainAxisSize: MainAxisSize.min,
+                      child: Text(
+                        totalDuration,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 13,
+                          color: Color.fromRGBO(245, 245, 245, 1),
+                          fontWeight: FontWeight.w400,
+                          fontFamily: 'ProximaNova',
+                        ),
+                      )),
+                ])),
+            const SizedBox(
+              height: 22,
+            ),
+            Container(
+              height: 40,
+              child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    if (isUnmatched)
+                      Container(
+                        color: const Color.fromRGBO(54, 57, 62, 1),
+                        height: 2,
+                        width: MediaQuery.of(context).size.width - 20,
+                      ),
+                    if (!isUnmatched)
+                      for (var i in amplitudes)
+                        Row(
+                          children: [
+                            Container(
+                              width: 2,
+                              height: (i!.max - i.current) * -1 / 4,
+                              margin: const EdgeInsets.only(bottom: 10),
+                              decoration: BoxDecoration(
+                                color: !isRecorded
+                                    ? const Color.fromRGBO(54, 57, 62, 0.95)
+                                    : const Color.fromRGBO(191, 189, 255, 1),
+                                borderRadius: BorderRadius.circular(999),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 1,
+                            ),
+                          ],
+                        ),
+                  ]),
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                for (var i in amplitudes)
-                  Row(
-                    children: [
-                      Container(
-                        width: 2,
-                        height: (i!.max - i.current) * -1 / 4,
-                        margin: const EdgeInsets.only(bottom: 10),
-                        decoration: BoxDecoration(
-                          color: !isRecorded
-                              ? const Color.fromRGBO(54, 57, 62, 0.95)
-                              : const Color.fromRGBO(191, 189, 255, 1),
-                          borderRadius: BorderRadius.circular(999),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 1,
-                      ),
-                    ],
-                  ),
-              ]),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: () async {
-                  submitDeletePressed();
-                },
-                child: Text(
-                  'Delete',
-                  style: TextStyle(
-                    color: areDeleteSubmitButtonEnabled == false
-                        ? const Color.fromRGBO(255, 255, 255, 0.3)
-                        : const Color.fromRGBO(255, 255, 255, 1),
-                    fontSize: 13,
-                    fontFamily: 'ProximaNova',
-                    fontWeight: FontWeight.w400,
+                TextButton(
+                  onPressed: () async {
+                    submitDeletePressed();
+                  },
+                  child: Text(
+                    'Delete',
+                    style: TextStyle(
+                      color: areDeleteSubmitButtonEnabled == false
+                          ? const Color.fromRGBO(255, 255, 255, 0.3)
+                          : const Color.fromRGBO(255, 255, 255, 1),
+                      fontSize: 13,
+                      fontFamily: 'ProximaNova',
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(width: 36),
-              IconButton(
-                onPressed: () {
-                  toggleButton();
-                },
-                icon: SvgPicture.asset('lib/assets/$buttonState.svg'),
-              ),
-              const SizedBox(width: 36),
-              TextButton(
-                onPressed: () {
-                  submitDeletePressed();
-                },
-                child: Text(
-                  'Submit',
-                  style: TextStyle(
-                    color: areDeleteSubmitButtonEnabled == false
-                        ? const Color.fromRGBO(255, 255, 255, 0.3)
-                        : const Color.fromRGBO(255, 255, 255, 1),
-                    fontSize: 13,
-                    fontFamily: 'ProximaNova',
-                    fontWeight: FontWeight.w400,
+                const SizedBox(width: 36),
+                IconButton(
+                  onPressed: () {
+                    toggleButton();
+                  },
+                  icon: SvgPicture.asset('lib/assets/$buttonState.svg'),
+                ),
+                const SizedBox(width: 36),
+                TextButton(
+                  onPressed: () {
+                    submitDeletePressed();
+                  },
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(
+                      color: areDeleteSubmitButtonEnabled == false
+                          ? const Color.fromRGBO(255, 255, 255, 0.3)
+                          : const Color.fromRGBO(255, 255, 255, 1),
+                      fontSize: 13,
+                      fontFamily: 'ProximaNova',
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          Text(
-            'Unmatch',
-            style: TextStyle(
-              color: isUnmatched == true
-                  ? const Color.fromRGBO(255, 41, 41, 1)
-                  : const Color.fromRGBO(190, 32, 32, 1),
-              fontSize: 13,
-              fontFamily: 'ProximaNova',
-              fontWeight: FontWeight.w400,
+              ],
             ),
-          ),
-        ],
-      ),
-    );
+            const SizedBox(
+              height: 22,
+            ),
+            Text(
+              'Unmatch',
+              style: TextStyle(
+                color: isUnmatched == true
+                    ? const Color.fromRGBO(255, 41, 41, 1)
+                    : const Color.fromRGBO(190, 32, 32, 1),
+                fontSize: 13,
+                fontFamily: 'ProximaNova',
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        ));
   }
 }
